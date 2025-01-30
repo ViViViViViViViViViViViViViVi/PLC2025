68533b058cec5a0e39284ec9f8c99f6865010ad9
V Jeyakanthan <Vithu.j3@outlook.com>
2025-01-30 14:44:09 +0000

    question 6 func edited

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,20 @@
---create inpFunc
-inpFunc = [1..5] 
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+inpFunc :: Int -> Int -> [Int]
+inpFunc a b = [a..b]
+
+
+applicatorFunc :: [Int] -> Char -> Double
+applicatorFunc inp s = 
+    if s == 's' 
+        then fromIntegral (sum inp) 
+        else fromIntegral (sum inp) / fromIntegral (length inp) 
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+    putStrLn "Enter the starting number (a):"
+    a <- readLn
+    putStrLn "Enter the ending number (b):"
+    b <- readLn
+    putStrLn "Enter 's' for sum or 'a' for average:"
+    op <- getLine
+    let result = applicatorFunc (inpFunc a b) (head op)  
+    putStrLn ("Result = " ++ show result)
